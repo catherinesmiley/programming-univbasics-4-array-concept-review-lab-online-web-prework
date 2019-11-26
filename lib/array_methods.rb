00d7454c0104805b76981371c8d65c1cc348c7af
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-26 04:30:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,11 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do 
-    array[count] == value_to_find
+    if array[count] == value_to_find
     count += 1
     return count
   end
+  
   nil
 end
 
